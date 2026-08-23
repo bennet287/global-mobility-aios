aa27ff0e66d5616d2509ebda2b9244a4e0dbf73b
bennet287 <77139673+bennet287@users.noreply.github.com>
2026-08-23 15:19:09 +0200

    add Cockpit refinement layout

---
+++ apps/web/app/cockpit/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import "./cockpit-refinements.css";
+
+export default function CockpitLayout({ children }: { children: ReactNode }) {
+  return children;
+}
