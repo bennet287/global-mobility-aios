66047ba86ec2816a25214c9f50b510034c584a55
bennet287 <77139673+bennet287@users.noreply.github.com>
2026-09-03 03:36:47 -0700

    feat(aios-v2): add Organization route

---
+++ apps/web/app/cockpit/v2/organization/page.tsx
@@ -0,0 +1,5 @@
+import { V2OrganizationWorkspace } from "../../../../components/v2/V2OrganizationWorkspace";
+
+export default function AiosV2OrganizationPage() {
+  return <V2OrganizationWorkspace />;
+}
